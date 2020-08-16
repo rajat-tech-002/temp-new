8c9658380e8aeb3ccbc4ffd8ee184a82fea861e5
Rajat <rajat.tech.002@gmail.com>
2020-08-16 23:41:02 +0530

    Updates

---
+++ precache-manifest.98cf03e0f150c36442ea7d50b8412c0c.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c32618f4eec882bd0987f7a94fb02ca",
+    "url": "/temp-new/index.html"
+  },
+  {
+    "revision": "df5becd80fbc3ffc2c88",
+    "url": "/temp-new/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "c971acecc49b4b1500a8",
+    "url": "/temp-new/static/css/main.8ac76785.chunk.css"
+  },
+  {
+    "revision": "df5becd80fbc3ffc2c88",
+    "url": "/temp-new/static/js/2.14a520ab.chunk.js"
+  },
+  {
+    "revision": "8390064e73dd8ad4676060139e575712",
+    "url": "/temp-new/static/js/2.14a520ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c971acecc49b4b1500a8",
+    "url": "/temp-new/static/js/main.64442d43.chunk.js"
+  },
+  {
+    "revision": "7e141d9cb284330d7ab2",
+    "url": "/temp-new/static/js/runtime-main.8b50cc73.js"
+  },
+  {
+    "revision": "4db339ea59bf1435ab66a89518d0898f",
+    "url": "/temp-new/static/media/anil.4db339ea.jpg"
+  },
+  {
+    "revision": "063e247ff1037ccb6cee58764c812572",
+    "url": "/temp-new/static/media/appdev.063e247f.jpg"
+  },
+  {
+    "revision": "a12e548471825f82dcb163fb500937dc",
+    "url": "/temp-new/static/media/b1.a12e5484.png"
+  },
+  {
+    "revision": "346a69d94c90318a76d6d12c64b943c2",
+    "url": "/temp-new/static/media/bhavani.346a69d9.jpg"
+  },
+  {
+    "revision": "c883e196fa9632694c15dd5c9b7ca6da",
+    "url": "/temp-new/static/media/dm.c883e196.jpg"
+  },
+  {
+    "revision": "131d60afb9d68a082c2cae0cd50a40b4",
+    "url": "/temp-new/static/media/eportal.131d60af.jpg"
+  },
+  {
+    "revision": "2b834b608766c9b8b3ad38e2a2f55747",
+    "url": "/temp-new/static/media/img10.2b834b60.gif"
+  },
+  {
+    "revision": "969e39e3bb8600ba8b2da098d561b00b",
+    "url": "/temp-new/static/media/img11.969e39e3.jpg"
+  },
+  {
+    "revision": "4a641b7e57af6f56576732291ef61c33",
+    "url": "/temp-new/static/media/img4.4a641b7e.gif"
+  },
+  {
+    "revision": "b2c3266750b658a41e124ccf6b4f36e6",
+    "url": "/temp-new/static/media/imgg2.b2c32667.jpg"
+  },
+  {
+    "revision": "1fa94d83d122c874127ad182fd57fcad",
+    "url": "/temp-new/static/media/imgg4.1fa94d83.jpg"
+  },
+  {
+    "revision": "d1b93a330eb9c5bb5923cfb674d92a6e",
+    "url": "/temp-new/static/media/imgg5.d1b93a33.jpg"
+  },
+  {
+    "revision": "35482a02e971baa926addbac17ff23a8",
+    "url": "/temp-new/static/media/kundansweets.35482a02.png"
+  },
+  {
+    "revision": "8eb8459afacacfff0fc398afba993c68",
+    "url": "/temp-new/static/media/laxmiherbals.8eb8459a.png"
+  },
+  {
+    "revision": "32427e8a743ffb34f739785612901468",
+    "url": "/temp-new/static/media/logo-yware.32427e8a.png"
+  },
+  {
+    "revision": "db9a850d0208493221b7b1c8c1780c6b",
+    "url": "/temp-new/static/media/lotushotel.db9a850d.jpg"
+  },
+  {
+    "revision": "d68bf1767d5c5eca28eb92285aa97460",
+    "url": "/temp-new/static/media/me2.d68bf176.png"
+  },
+  {
+    "revision": "025516441e581c4b17185572371c2e8f",
+    "url": "/temp-new/static/media/new1.02551644.jpg"
+  },
+  {
+    "revision": "b614928b96b22c5c879fb1e28fa867ea",
+    "url": "/temp-new/static/media/prateek.b614928b.jpg"
+  },
+  {
+    "revision": "a916c01f1558e9550c07d4266c70fdbc",
+    "url": "/temp-new/static/media/raghav.a916c01f.jpg"
+  },
+  {
+    "revision": "c193913220bc51e1d8c91a2669216a80",
+    "url": "/temp-new/static/media/sachintraders.c1939132.png"
+  },
+  {
+    "revision": "993b6516cc3ebc78b64aa5a9a301cc63",
+    "url": "/temp-new/static/media/sanskarayush.993b6516.png"
+  },
+  {
+    "revision": "118762ef00972e421a12b0c7b4e8f679",
+    "url": "/temp-new/static/media/sde.118762ef.jpg"
+  },
+  {
+    "revision": "f20d1faf3b76a878434ccb3b4f3ff308",
+    "url": "/temp-new/static/media/streetviewlounge.f20d1faf.png"
+  },
+  {
+    "revision": "101762d59e5e950346775493b6a07ab6",
+    "url": "/temp-new/static/media/vasu.101762d5.jpg"
+  },
+  {
+    "revision": "55f595392044dd2edc7cf9b706c2b949",
+    "url": "/temp-new/static/media/webdev.55f59539.jpg"
+  },
+  {
+    "revision": "33b4ca2b4e92d72d1e5696dc254d868e",
+    "url": "/temp-new/static/media/yash.33b4ca2b.jpeg"
+  }
+]);
